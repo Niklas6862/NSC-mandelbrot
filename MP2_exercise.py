1af198a722fd1d0c20296777327a023927134b15
Niklas K Andersen <[nka@bbsyd.dk]>
2026-03-09 12:24:46 +0100

    Renamed "comparison..." to "MP1". started exercises from lecture 4

---
+++ MP2_exercise.py
@@ -0,0 +1,26 @@
+import random, math, time, statistics
+from multiprocessing import Pool
+import os, random, time, statistics
+
+def estimate_pi_serial(num_samples):
+    inside_circle = 0
+    for _ in range(num_samples):
+        x, y = random.random(), random.random()
+        if x*x + y*y <= 1:
+            inside_circle += 1
+    return 4 * inside_circle / num_samples
+
+def estimate_pi_chunk(num_samples):
+    inside_circle = 0
+    for _ in range(num_samples):
+        x, y = random.random(), random.random()
+        if x*x + y*y <= 1:
+            inside_circle += 1
+    return inside_circle
+
+def estimate_pi_parallel(num_samples, num_processes=4):
+    samples_per_process = num_samples // num_processes
+    tasks = [samples_per_process] * num_processes
+    with Pool(processes=num_processes) as pool:
+        results = pool.map(estimate_pi_chunk, tasks)
+    return 4 * sum(results) / num_samples
